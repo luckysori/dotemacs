adc49e84967fe4dd59af94601678392cfe06e6c5
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2020-01-19 23:55:54 +1100

    Reorder hippie expand try function list
    No longer will it try to expand to list or line at first.

--- custom.el
+++ custom.el
@@ -62,33 +62,64 @@
                            ("#F2804F" . 70)
                            ("#F771AC" . 85)
                            ("#eee8d5" . 100)))
+ '(hippie-expand-try-functions-list '(try-complete-file-name-partially try-complete-file-name
+                                                                       try-expand-all-abbrevs
+                                                                       try-expand-dabbrev
+                                                                       try-expand-list
+                                                                       try-expand-line
+                                                                       try-expand-dabbrev-all-buffers
+                                                                       try-expand-dabbrev-from-kill
+                                                                       try-complete-lisp-symbol-partially
+                                                                       try-complete-lisp-symbol))
  '(hl-bg-colors '("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342"))
  '(hl-fg-colors '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
  '(input-method-highlight-flag nil)
  '(magit-diff-use-overlays nil)
- '(org-agenda-files '("~/work/org/tasks.org" "~/personal/general.org" "~/work/org/retro.org"
-                      "~/work/org/scrum.org"))
- '(package-selected-packages '(helm-fuzzier tide typescript-mode lsp-ui helm-ag latex-mode
-                                            LaTeX-mode company-latex latex-preview-pane
-                                            company-auctex async forge dired-git-info dired diredfl
-                                            volume restclient indium yaml-mode helm-org
-                                            helm-projectile projectile adoc-mode dap-rust dap-mode
-                                            helm-lsp company-lsp docker-tramp rjsx-mode
-                                            flycheck-color-mode-line elfeed helm-rg json-mode avy
-                                            smart-parens hippie hippie-expand electric-pair-mode
-                                            electric-pair erc-highlight-nicknames comint-mode comint
-                                            visible-bell-mode visible-bell ibuffer-mode prettier
-                                            prettier-js helm-swoop elisp-format helm-flx try
-                                            winner-mode helm-ls-git wgrep-helm add-node-modules-path
-                                            company-quickhelp flymd exec-path-from-shell wget
-                                            js2-mode diff-hl browse-kill-ring cargo company
-                                            rust-mode haskell-mode which-key cl-lib multiple-cursors
-                                            expand-region ace-window intero hindent emojify
-                                            smooth-scrolling auctex pdf-tools geiser))
+ '(mc/always-run-for-all 1)
+ '(org-agenda-files '("~/org/general.org" "~/work/org/tasks.org"))
+ '(org-download-display-inline-images nil)
+ '(org-export-with-tasks 'done)
+ '(org-tags-column 0)
+ '(org-todo-keyword-faces '(("TODO" . org-warning)
+                            ("IN-PROGRESS" . "yellow")
+                            ("BLOCKED" . "dim gray")
+                            ("DONE" . org-done)
+                            ("CANCELLED" . "blue")))
+ '(org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "BLOCKED" "DONE" "CANCELLED")))
+ '(package-selected-packages '(org-noter helm-fuzzier tide typescript-mode lsp-ui helm-ag latex-mode
+                                         LaTeX-mode company-latex latex-preview-pane company-auctex
+                                         async forge dired-git-info dired diredfl restclient indium
+                                         yaml-mode helm-org helm-projectile projectile adoc-mode
+                                         dap-rust dap-mode helm-lsp company-lsp docker-tramp
+                                         rjsx-mode flycheck-color-mode-line elfeed helm-rg json-mode
+                                         avy smart-parens hippie-expand electric-pair-mode
+                                         electric-pair erc-highlight-nicknames comint-mode comint
+                                         visible-bell-mode visible-bell ibuffer-mode prettier
+                                         prettier-js helm-swoop elisp-format helm-flx try
+                                         winner-mode helm-ls-git wgrep-helm add-node-modules-path
+                                         company-quickhelp flymd exec-path-from-shell wget js2-mode
+                                         diff-hl browse-kill-ring cargo company rust-mode
+                                         haskell-mode which-key cl-lib multiple-cursors
+                                         expand-region ace-window intero hindent emojify auctex
+                                         pdf-tools geiser))
  '(pos-tip-background-color "#eee8d5")
  '(pos-tip-foreground-color "#586e75")
+ '(preview-gs-command "/usr/bin/gs" t)
+ '(preview-image-type 'pnm t)
+ '(reftex-mode t t)
  '(set-face-attribute 'helm-source-header t)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2)))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(web-mode-block-padding 2 t)
+ '(web-mode-code-indent-offset 2 t)
+ '(web-mode-comment-style 2 t)
+ '(web-mode-css-indent-offset 2 t)
+ '(web-mode-enable-auto-indentation t t)
+ '(web-mode-enable-auto-pairing t t)
+ '(web-mode-enable-auto-quoting nil t)
+ '(web-mode-enable-comment-keywords t t)
+ '(web-mode-enable-css-colorization t t)
+ '(web-mode-enable-current-element-highlight t t)
+ '(web-mode-markup-indent-offset 2 t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
